ac13ef6d439a7f2a1fc0c5983c58b321bfe2a730
fgborja <xicoborja@gmail.com>
2014-07-29 19:30:24 -0300

    Refactor adjusted tabs and deletion of commented lines

--- public/javascripts/rollCallsTimeLine.js
+++ public/javascripts/rollCallsTimeLine.js
@@ -1,121 +1,3 @@
-
-// if(!d3.chart) d3.chart = {};
-
-// d3.chart.rollCallsTimeLine = function() {
-// 	var timeline, g;s
-// 	var data;
-// 	var width = 1000;
-// 	var height = 50;
-// 	var dispatch = d3.dispatch(chart, "filter");
-
-// 	function chart(container) {
-// 		timeline = container.append('svg:svg');
-// 	g = timeline.append('g')
-// 	g.attr("transform", "translate(" + [70, 0] + ")")
-		
-// 	}
-// 	chart.update = update;
-// 	function update() {
-// 		var extent = d3.extent(data, function(d) {
-// 			return d.datetime
-// 		})wew
-
-// 		var scale = d3.time.scale()
-// 			.domain(extent)
-// 			.range([0, width])
-
-// 		var brush = d3.svg.brush()
-// 		brush.x(scale)
-// 		brush(g)
-// 		g.selectAll("rect").attr("height", height)
-// 		g.selectAll(".background")
-// 			.style({fill: "#4B9E9E", visibility: "visible"})
-// 		g.selectAll(".extent")
-// 			.style({fill: "#78C5C5", visibility: "visible"})
-// 		g.selectAll(".resize rect")
-// 			.style({fill: "#276C86", visibility: "visible"})
-
-// 		var rects = g.selectAll("rect.events")
-// 		.data(data)
-// 		rects.enter()
-// 		.append("rect").classed("events", true)
-// 		rects.attr({
-// 			x: function(d) { return scale(d.datetime);},
-// 			y: 0,
-// 			width: 1,
-// 			height: height
-// 		}).style("pointer-events", "none")
-
-// 		rects.exit().remove()
-
-// 		brush.on("brushend", function() {
-// 			var ext = brush.extent()
-// 			var filtered = data.filter(function(d) {
-// 				return (d.datetime > ext[0] && d.datetime < ext[1])
-// 			})
-// 			g.selectAll("rect.events")
-// 			.style("stroke", "")
-			
-// 			// g.selectAll("rect.events")
-// 			// .data(filtered, function(d) { return d.data.id })
-// 			// .style({
-// 			//   stroke: "#fff"
-// 			// })
-
-// 			//emit filtered data
-// 			dispatch.filter(filtered)
-// 		})
-
-// 		var axis = d3.svg.axis()
-// 		.scale(scale)
-// 		.orient("bottom")
-// 		.tickValues([new Date(extent[0]), new Date(extent[0] + (extent[1] - extent[0])/2) , new Date(extent[1])])
-// 		.tickFormat(d3.time.format("%x %H:%M"))
-
-// 		var agroup = g.append("g")
-// 		agroup.attr("transform", "translate(" + [0, height] + ")")
-// 		axis(agroup)
-// 		agroup.selectAll("path")
-// 			.style({ fill: "none", stroke: "#000"})
-// 		agroup.selectAll("line")
-// 			.style({ stroke: "#000"})
-// 	}
-
-// 	// chart.highlight = function(data) {
-// 	//   var rects = g.selectAll("rect.events")
-// 	//   .style("stroke", "")
-// 	//   .style("stroke-width", "")
-
-// 	//   rects.data(data, function(d) { return d.data.id })
-// 	//   .style("stroke", "orange")
-// 	//   .style("stroke-width", 3)
-// 	// }
-
-// 	chart.data = function(value) {
-// 		if(!arguments.length) return data;
-// 		data = value;
-// 		return chart;
-// 	}
-// 	chart.width = function(value) {
-// 		if(!arguments.length) return width;
-// 		width = value;
-// 		return chart;
-// 	}
-// 	chart.height = function(value) {
-// 		if(!arguments.length) return height;
-// 		height = value;
-// 		return chart;
-// 	}
-
-// 	return d3.rebind(chart, dispatch, "on");
-// }
-
-
-//===============================================================================================================
-//===============================================================================================================
-//===============================================================================================================
-// TODO create an g element for the ranges+timeline 
-
 if(!d3.chart) d3.chart = {};
 
 d3.chart.timelineBarChart = function() {
@@ -303,68 +185,68 @@ d3.chart.timelineBarChart = function() {
 	});
 
 	brush.on("brushend.chart", function() {
-	  if (brush.empty()) {
-		var div = d3.select(this.parentNode.parentNode.parentNode);
-		div.select(".title a").style("display", "none");
-		div.select("#clip-timeline rect").attr("x", null).attr("width", "100%");
-		//dimension.filterAll();
-	  }
-	  // clear selected alliance
-	  dispatch.setAlliances(null);
-	  svg.select('.glyphicon.selected').classed('selected',false);
-	  //!!!!!!!!!!
-	  dispatch.timelineFilter(brush.extent())
-	  //console.log()
+		if (brush.empty()) {
+			var div = d3.select(this.parentNode.parentNode.parentNode);
+			div.select(".title a").style("display", "none");
+			div.select("#clip-timeline rect").attr("x", null).attr("width", "100%");
+			//dimension.filterAll();
+		}
+		// clear selected alliance
+		dispatch.setAlliances(null);
+		svg.select('.glyphicon.selected').classed('selected',false);
+		//!!!!!!!!!!
+		dispatch.timelineFilter(brush.extent())
+		//console.log()
 	});
 
 	chart.margin = function(_) {
-	  if (!arguments.length) return margin;
-	  margin = _;
-	  return chart;
+		if (!arguments.length) return margin;
+		margin = _;
+		return chart;
 	};
 
 	chart.x = function(_) {
-	  if (!arguments.length) return x;
-	  x = _;
-	  axis.scale(x);
-	  brush.x(x);
-	  return chart;
+		if (!arguments.length) return x;
+		x = _;
+		axis.scale(x);
+		brush.x(x);
+		return chart;
 	};
 
 	chart.y = function(_) {
-	  if (!arguments.length) return y;
-	  y = _;
-	  return chart;
+		if (!arguments.length) return y;
+		y = _;
+		return chart;
 	};
 
 	chart.dimension = function(_) {
-	  if (!arguments.length) return dimension;
-	  dimension = _;
-	  return chart;
+		if (!arguments.length) return dimension;
+		dimension = _;
+		return chart;
 	};
 
 	chart.timelineFilter = function(_) {
-	  if (_) {
-		brush.extent(_);
-		dimension.filterRange(_);
-	  } else {
-		brush.clear();
-		dimension.filterAll();
-	  }
-	  brushDirty = true;
-	  return chart;
+		if (_) {
+			brush.extent(_);
+			dimension.filterRange(_);
+		} else {
+			brush.clear();
+			dimension.filterAll();
+		}
+		brushDirty = true;
+		return chart;
 	};
 
 	chart.group = function(_) {
-	  if (!arguments.length) return group;
-	  group = _;
-	  return chart;
+		if (!arguments.length) return group;
+		group = _;
+		return chart;
 	};
 
 	chart.round = function(_) {
-	  if (!arguments.length) return round;
-	  round = _;
-	  return chart;
+		if (!arguments.length) return round;
+		round = _;
+		return chart;
 	};
 
 	chart.data = function(value) {
